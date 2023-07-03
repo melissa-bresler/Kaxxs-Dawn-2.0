8fbbb58badd7d745ebcb50340e4fab56cd8f58c5
melissa-bresler <108290826+melissa-bresler@users.noreply.github.com>
2023-07-03 15:56:40 +0200

    Environment added
    -Player no longer floats
    -Started working on multiple cameras and triggering them through player movement.

---
+++ Assets/Scripts/CameraTriggerVolume.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTriggerVolume : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private Vector3 boxSize;
+
+    BoxCollider box;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        box = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        box.isTrigger = true;
+        box.size = boxSize;
+
+        rb.isKinematic = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, boxSize);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (CameraSwitcher.ActiveCamera != cam) CameraSwitcher.SwitchCamera(cam);
+        }
+    }
+}
